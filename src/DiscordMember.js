47ad63683290893c2ca0bfa0511c6785b7144b5f
ImDexHD <imdexhdyt@gmail.com>
2017-09-14 23:31:16 +0300

    Add files via upload

---
+++ src/DiscordMember.js
@@ -0,0 +1,205 @@
+const config        = require('./data/client.json')
+const Util          = require('./Util')
+
+const request       = require('request-promise').defaults({pool: {maxSockets: Infinity}});
+
+let DiscordServer;
+
+module.exports =
+// A representation of a Discord guild member.
+class DiscordMember {
+    constructor(discordServer, id) {
+        this.discordServer = discordServer;
+        this.id = id;
+
+        this.bot = this.discordServer.bot;
+        this.server = this.discordServer.server;
+
+        // Have to do this to prevent circular reference in file requires
+        // Gets a reference to DiscordServer so we can run static methods
+        DiscordServer = this.discordServer.constructor;
+    }
+
+    static async new(discordServer, id) {
+        let discordMember = new DiscordMember(discordServer, id);
+
+        if (!discordMember.prepareMember()) {
+            return false;
+        }
+
+        return discordMember;
+    }
+
+    async prepareMember() {
+        try {
+            this.user = await this.bot.fetchUser(this.id, false);
+            this.member = await this.server.fetchMember(this.user, false);
+            return true;
+        } catch (e) {
+            if (config.loud) console.log(`prepareMember: ${e.message}; ${this.id}; ${this.user}`);
+            return false;
+        }
+    }
+
+    // Gets a member's nickname, formatted with this server's
+    // specific settings.
+    getNickname(data) {
+        return Util.formatDataString(this.discordServer.getSetting('nicknameFormat'), data, this.member);
+    }
+
+    // This method is called to update the state of a specific member
+    // in this Discord server.
+    async verify(options) {
+        var options = options || {};
+        let data = {};
+        let freshData = false;
+
+        if (!this.member && !await this.prepareMember()) {
+            return {
+                status: false,
+                error: "User not in guild.",
+                nonFatal: true
+            }
+        }
+
+        try {
+            // Read user data from memory, or request it if there isn't any cached.
+            data = await Cache.get("users", this.id);
+            if (!data) {
+                data =  await request({
+                    uri: `https://verify.eryn.io/api/user/${this.id}`,
+                    json: true,
+                    simple: false
+                });
+                freshData = true;
+            }
+        } catch (e) {
+            if (config.loud) console.log(e);
+            return {
+                status: false,
+                error: "Unknown error."
+             }
+        }
+
+        // If the status is ok, the user is in the database.
+        if (data.status === "ok") {
+
+            // We only want to update the username if this data isn't from the cache.
+            if (freshData) {
+                // Resolve the Roblox username from the user id.
+                let apiUserData = {}
+                try {
+                    apiUserData = await request({
+                        uri: `http://api.roblox.com/users/${data.robloxId}`,
+                        json: true,
+                        simple: false
+                    });
+                } catch (e) {
+                    return false;
+                }
+
+                if (apiUserData.Username) {
+                    data.robloxUsername = apiUserData.Username;
+                }
+            }
+
+            // Cache the data for future use.
+            Cache.set("users", this.id, data);
+
+            try {
+
+                // Check if these settings are enabled for this specific server,
+                // if so, then put the member in the correct state.
+
+                if (this.discordServer.getSetting('nicknameUsers')) {
+                    await this.member.setNickname(this.getNickname(data));
+                }
+
+                if (this.discordServer.getSetting('verifiedRole')) {
+                    await this.member.addRole(this.discordServer.getSetting('verifiedRole'));
+                }
+                
+                if (this.discordServer.getSetting('verifiedRemovedRole')){
+                    await this.member.removeRole(this.discordServer.getSetting('verifiedRemovedRole'));
+                }
+
+                if (this.discordServer.getSetting('announceChannel') && options.announce !== false) {
+                    let channel = await this.server.channels.get(this.discordServer.getSetting('announceChannel'));
+
+                    if (channel) {
+                        channel.send(`**User verified:** <@${this.id}> as ${data.robloxUsername}`);
+                    }
+                }
+
+                // Check if we want to resolve group rank bindings with cached or fresh data.
+                if (options.clearBindingsCache !== false) {
+                    await Cache.clear(`bindings.${data.robloxId}`);
+                }
+
+                // Resolve group rank bindings for this member.
+                if (this.discordServer.getSetting('groupRankBindings').length > 0) {
+                    for (let binding of this.discordServer.getSetting('groupRankBindings')) {
+                        // We use a Promise.then here so that they all execute asynchronously. 
+                        DiscordServer.resolveGroupRankBinding(binding, data.robloxId, data.robloxUsername)
+                            .then((state) => {
+                                if (state === true) {
+                                    this.member.addRole(binding.role);
+                                } else {
+                                    this.member.removeRole(binding.role);
+                                }
+                            })
+                            .catch((e) => {
+                                if (config.loud) console.log(e);
+                                console.log('Resolution error for binding');
+                            });
+                    }
+                }
+            } catch (e) {
+                if (config.loud) console.log(e);
+                // If anything failed here, it's most likely because the bot
+                // couldn't modify the member due to a permission problem.
+                return {
+                    status: false,
+                    nonFatal: true,
+                    error: "RoVer couldn't modify the member in this server. Either the bot doesn't have permission or the target user cannot be modified by the bot (such as higher rank in the server)."
+                }
+            }
+
+            return {
+                status: true,
+                robloxUsername: data.robloxUsername,
+                robloxId: data.robloxId,
+                discordId: this.member.id,
+                discordName: this.member.user.username
+            }
+        } else {
+            // Status was not "ok".
+             switch (data.errorCode){
+                case 404:
+                    // User isn't in the database.
+
+                    // Add the "Not Verified" role to the user.
+                    if (this.discordServer.getSetting('verifiedRemovedRole')){
+                        await this.member.addRole(this.discordServer.getSetting('verifiedRemovedRole'));
+                    }
+
+                    return {
+                        status: false,
+                        error: "You don't seem to be verified! Please go to https://verify.eryn.io and follow the instructions on the page in order to get verified."
+                    }
+                case 429: 
+                    // This client has exceeded the amount of requests allowed in a 60 second period.
+                    return {
+                        status: false,
+                        error: "Server is busy. Please try again later."
+                    }
+                default:
+                    // Something else is wrong.
+                    return {
+                        status: false,
+                        error: "Unknown error."
+                    }
+            }
+        }
+    }
+}
---
+++ src/VirtualGroups.js
@@ -0,0 +1,110 @@
+const path        = require('path')
+const fs          = require('fs')
+const Discord     = require('discord.js')
+const request     = require('request-promise')
+const config      = require('./data/client.json')
+
+// VirtualGroups can be used in place of group IDs for
+// group bindings. They are defined as keys in the
+// VirtualGroups object. It must be a function that
+// returns true or false. 
+module.exports = {
+    // Check if the given user is in the Roblox Dev Forum.
+    // userid: ROBLOX user id.
+    async DevForum(user) {
+        let userid = user.id;
+        let username = user.username;
+
+        // Fetch the DevForum data for this user.
+        let devForumData = {}
+        
+        try {
+            devForumData = await request({
+                uri: `http://devforum.roblox.com/users/${username}.json`,
+                json: true,
+                simple: false
+            });
+        } catch (e) {
+            return false;
+        }
+        
+        try {
+            // If the trust_level in the user data is above 0, then they are a member.
+            if (devForumData.user.trust_level > 0) {
+                return true;
+            }
+        } catch(e) {
+            return false;
+        }
+
+        return false;
+    },
+
+    async HasAsset(user, assetid) {
+        let userid = user.id;
+        try {
+            let responseData = await request({
+                uri: `http://api.roblox.com/ownership/hasasset?userId=${userid}&assetId=${assetid}`,
+                simple: false
+            });
+
+            if (responseData === 'true') {
+                return true;
+            }
+        } catch (e) {
+            // do nothing
+        }
+
+        return false;
+    },
+
+    async BuildersClub(user, bcType) {
+        let bc = await Cache.get(`bindings.${user.id}`, 'bc');
+        // TODO: Fix the caching on this. Currently, as all bindings execute at the same time,
+        // this won't actually ever cache because they are all requesting. 
+        if (!bc) {
+            let response = await request({
+                uri: `https://www.roblox.com/Thumbs/BCOverlay.ashx?username=${user.username}`,
+                simple: false,
+                resolveWithFullResponse: true
+            });
+
+            let url = response.request.uri.href;
+            bc = "NBC";
+
+            if (url.includes("overlay_obcOnly")) {
+                bc = "OBC";
+            } else if (url.includes("overlay_tbcOnly")) {
+                bc = "TBC";
+            } else if (url.includes("overlay_bcOnly")) {
+                bc = "BC";
+            }
+
+            Cache.set(`bindings.${user.id}`, 'bc', bc);
+        }
+
+        if (bcType && bcType === bc) {
+            return true;
+        } else if (!bcType && bc !== "NBC") {
+            return true;
+        }
+
+        return false;
+    },
+
+    async BC(user) {
+        return await module.exports.BuildersClub(user, "BC");
+    },
+
+    async TBC(user) {
+        return await module.exports.BuildersClub(user, "TBC");
+    },
+
+    async OBC(user) {
+        return await module.exports.BuildersClub(user, "OBC");
+    },
+
+    async NBC(user) {
+        return await module.exports.BuildersClub(user, "NBC");
+    },
+}
